95fd139bef83e7b4a26221f015873b7f35ffdd9f
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2025-06-12 10:44:48 +0200

    update email messages

--- src/utilities/emails.ts
+++ src/utilities/emails.ts
@@ -9,6 +9,17 @@ export const welcome = {
     el : "Γεια σας, καλώς ήρθατε στην Pictalk! Εδώ είναι το σύμβολο επικύρωσης."
 }
 
+export const validAccount = {
+    en: "Your account is now valid, you can now log in",
+    fr: "Votre compte est maintenant valide, vous pouvez vous connecter",
+    es: "Su cuenta ya es válida, ahora puede iniciar sesión",
+    it: "Il tuo account è ora valido, puoi accedere",
+    de: "Ihr Konto ist jetzt gültig, Sie können sich jetzt anmelden",
+    ro: "Contul dvs. este acum valid, vă puteți conecta",
+    po: "Sua conta agora é válida, você pode fazer login",
+    el: "Ο λογαριασμός σας είναι πλέον έγκυρος, μπορείτε να συνδεθείτε"
+}
+
 export const resetPassword = {
     en : "reset password",
     fr : "réinitialiser le mot de passe",
